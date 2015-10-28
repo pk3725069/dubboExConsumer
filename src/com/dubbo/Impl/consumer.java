211e1a0697451b3563b1bd9c73ad7acf4632d024
pk3725069 <lhzh2qh@163.com>
2015-10-28 22:06:30 +0800

    20151028 新建Consumer Demo

---
+++ src/com/dubbo/Impl/consumer.java
@@ -0,0 +1,21 @@
+package com.dubbo.Impl;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.dubbo.HelloService;
+
+public class consumer {
+
+	public static void main(String[] args) throws Exception {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				new String[] { "consumer.xml" });
+		context.start();
+
+		HelloService helloService = (HelloService) context.getBean("helloService");
+		String hello = helloService.sayHello("tom");
+		System.out.println(hello);
+
+		System.in.read();
+	}
+
+}
