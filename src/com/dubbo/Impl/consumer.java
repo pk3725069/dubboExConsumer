af245242ea2efad3ffd234a732627ea6f88caf19
pk3725069 <lhzh2qh@163.com>
2015-11-02 15:05:34 +0800

    20151102 参数回调函数

--- src/com/dubbo/Impl/consumer.java
+++ src/com/dubbo/Impl/consumer.java
@@ -2,23 +2,29 @@ package com.dubbo.Impl;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.dubbo.HelloService;
+import com.dubbo.CallbackListener;
+import com.dubbo.CallbackService;
+
 
 public class consumer {
-
+	static int time=0;
 	public static void main(String[] args) throws Exception {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				new String[] { "consumer.xml" });
-		context.start();
 
-		HelloService helloService = (HelloService) context.getBean("feedbackIndexService");
-		String hello = helloService.sayHello("tom");
-		System.out.println(hello);
-		
-		 helloService = (HelloService) context.getBean("memberIndexService");
-		 hello = helloService.sayHello("tom");
-		System.out.println(hello);
-		System.in.read();
+
+        context.start();
+        CallbackService callbackService = (CallbackService) context.getBean("callbackService");
+        callbackService.addListener( new CallbackListener() {
+            public void changed(String msg) {
+            	time++;
+                System.out.println("callback:"+time + msg);
+            }
+        }, "foo.bar");
+       
+
+        
+        System.in.read();
 	}
 
 }
